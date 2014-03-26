4ccacfca496d160f89c393ae6d4523a5b1882a46
kaminglong <kaminglong@gmail.com>
2014-03-26 05:23:46 +0000

    Deleting So I can commit code after (Should be in a few seconds)

--- bscah-homebase/tests/testdbProjects.php
+++ bscah-homebase/tests/testdbProjects.php
@@ -1,22 +1,8 @@
 <?php
 /*
  * Created on Mar 18, 2014
+ * Updated 3/26
  * @author Derek and Ka Ming
  */
-include_once(dirname(__FILE__).'/../database/dbProjects.php');
-include_once(dirname(__FILE__).'/../database/dbDates.php');
-class testdbProjects extends UnitTestCase {
-  function testdbProjectsModule() {
-	$p1=new Project("02-25-08-15-18","Rec", 3, array(), array(), "", "");
-	$this->assertTrue(insert_dbProjects($p1));
-	$this->assertTrue(delete_dbProjects($p1));
-	$p2=new Project("02-25-08-15-18","Rec",3, array(), array(), "", "");
-	$this->assertTrue(insert_dbProjects($p2));
-	$p2=new Project("02-25-08-15-18","Rec",2, array(), array(), "", "");
-	$this->assertTrue(update_dbProjects($p2));
-	$projects[] = $p2;
-	$this->assertTrue(delete_dbProjects($p2));
-	echo ("testdbProjects complete");
-  }
-}
+
 ?>
