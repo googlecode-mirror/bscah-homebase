8d74c6bf5b2b47acce78e8c3917cccf02464c5aa
kaminglong <kaminglong@gmail.com>
2014-03-26 05:26:46 +0000

    Derek was unable to commit the code, so he emailed it to me so I can commit it for him -Ka Ming

--- bscah-homebase/tests/testProject.php
+++ bscah-homebase/tests/testProject.php
@@ -5,7 +5,61 @@
  * Updated 3/26
  * @author Derek and Ka Ming
  */
+include_once(dirname(__FILE__).'/../domain/Project.php');
+class testProject extends UnitTestCase {
+      function testProjectModule() {
+         $m = new Project("03-28-08-12-15", "Rec", 3, array(), array(), "", "");
+ 
 
+    echo 'will test ProjectModule </br>';
+    $result = ProjectModule($m);
+    echo 'result is ' . $result;
+    if ($result)
+        echo "ProjectModule succeeded </br>";
+    else
+        echo "ProjectModule failed</br>";
+    }
+         
+// Test new function for resetting project's start/end time
+       function resetTime() {
+         $m = RrsetTime("11" , "13", array(), array(), "", "");
+ 
+
+    echo 'will reset Projects start/end time</br>';
+    $result = resetTime($m);
+    echo 'result is ' . $result;
+    if ($result)
+        echo "Resetting project start/end time succeeded </br>";
+    else
+        echo "Resetting project start/end time failed</br>";
+    }
+       		 
+// Test new function to catch invalid times
+       function invalidTime() {
+         $m = invalidTime("25" , "35", array(), array(), "", "");
+ 
+
+    echo 'will catch an Invalid Time and give an error message</br>';
+    $result = invalidTime($m);
+    echo 'result is ' . $result;
+    if ($result)
+        echo "Invalid time was caught </br>";
+    else
+        echo "Invalid time was not caught </br>";
+    }
+       
+		
+// test if the actual result equals the expected result
+function checkEquals($result, $expected) {
+    if ($result == $expected) {
+        echo 'result ' . $result . ' is the same as expected value ' . $expected . '</br>';
+        return true;
+    } else {
+        echo 'result ' . $result . ' does not equal the  expected value ' . $expected . '</br>';
+        return false;
+     }
+    }
+}
 
 ?>
 
--- bscah-homebase/tests/testdbProjects.php
+++ bscah-homebase/tests/testdbProjects.php
@@ -4,5 +4,93 @@
  * Updated 3/26
  * @author Derek and Ka Ming
  */
+include_once(dirname(__FILE__) . '/../domain/Project.php');
+echo 'testing dbProjects.php</br>';
+testAddProject();
+testRetrieveProject();
 
+//testGetall_dbProjects();
+
+echo("testdbProjects complete</br>");
+
+// tests the add_project() function in dbProjects.php
+function testAddProject() {
+    $m = new Project("Project 1" , "01-12-13", "13", "18");
+
+    echo 'will test add_project </br>';
+    $result = add_project($m);
+    echo 'result is ' . $result;
+    if ($result)
+        echo "add_project succeeded </br>";
+    else
+        echo "add_project failed</br>";
+
+    $res = remove_project("Project1");
+    if ($res == null)
+        echo 'Retrieve failed</br>';
+}
+
+// tests the retrieve_project() function in dbProjects.php
+function testRetrieveProject() {
+    $m = new Project("Project 1", "01-12-13", "13", "18");
+
+    echo 'will test retrieve_project </br>';
+    $result = add_project($m);
+    echo 'result is ' . $result;
+    if ($result)
+        echo "add_project succeeded </br>";
+    else
+        echo "add_project failed</br>";
+
+
+    $p = retrieve_project("Project1");
+    if ($p == null)
+        echo 'Retrieve failed</br>';
+    else {
+        checkEquals($p->get_status(), "active");
+       
+  
+    }
+
+    $res = remove_project("Project1");
+    if ($res == null)
+        echo 'Retrieve failed</br>';
+}
+
+// tests the retrieve_projects_by_name() function in dbProjects.php
+function testRetrieve_projects_by_name() {
+    $m = new Project("Project1" , "01-12-13", "13", "18");
+
+    echo 'will test retrieve_projects_by_name </br>';
+    $result = add_project($m);
+    echo 'result is ' . $result;
+    if ($result)
+        echo "add_project succeeded </br>";
+    else
+        echo "add_project failed</br>";
+
+    echo "test retrieve_projects_by_name</br>";
+    $projectList = retrieve_projects_by_name("Project1");
+    if ($projectList == null)
+        echo 'Retrieve failed</br>';
+    else {
+        checkEquals($projectList[0]->get_status(), "active");
+     
+    }
+
+    $res = remove_project("Project1");
+    if ($res == null)
+        echo 'Retrieve failed</br>';
+}  
+
+// test if the actual result equals the expected result
+function checkEquals($result, $expected) {
+    if ($result == $expected) {
+        echo 'result ' . $result . ' is the same as expected value ' . $expected . '</br>';
+        return true;
+    } else {
+        echo 'result ' . $result . ' does not equal the  expected value ' . $expected . '</br>';
+        return false;
+    }
+}
 ?>
