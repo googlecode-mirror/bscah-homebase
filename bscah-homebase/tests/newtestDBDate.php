ac3208994f36de789f8f1ee1e4d31e17877049b5
jimmy.loeffler.21 <jimmy.loeffler.21@gmail.com>
2014-04-01 21:24:03 +0000

    new test db date is finished

--- bscah-homebase/tests/newtestDBDate.php
+++ bscah-homebase/tests/newtestDBDate.php
@@ -3,20 +3,22 @@ include_once(dirname(__FILE__) . '/../domain/BSCAHdate.php');
 include_once(dirname(__FILE__) . '/../database/dbDates.php');
 echo 'testing dbDates.php</br>';
 error_log("in test db dates");
+
 test_insert_dbDates();
 test_update_dbDates();
 test_replace_dbDates();
 test_select_dbDates();
+test_get_shifts_text();
 // need more tests!
 
 echo("test complete</br>");
 
 // tests the insert_dbDates() function in dbDates.php
 function test_insert_dbDates() {
-   
     // note - I do not know what the date ids are going to look like. Change this when
     // you know how they will be formatted
    $newDate = new BSCAHdate("02-24-14", "test", "notes", "p1*p2");
+   echo 'test</br>';
     echo 'will test insert_dbDates </br>';
     $result = insert_dbDates($newDate);
     echo 'result is ' . $result;
@@ -25,7 +27,7 @@ function test_insert_dbDates() {
     else
         echo "insert_dbDate failed</br>";
 
-    $res = delete_dbDates("id1");
+    $res = delete_dbDates($newDate);
     if ($res == null)
         echo 'Delete failed</br>';
 }
@@ -42,7 +44,7 @@ function test_update_dbDates(){
     else
         echo "update_dbDate failed</br>";
 
-    $res = delete_dbDates("id1");
+    $res = delete_dbDates($newDate);
     if ($res == null)
         echo 'Delete failed</br>';
 }
@@ -58,6 +60,12 @@ function test_replace_dbDates(){
     
     if ($result != true)
         echo 'replace_dbDates failed</br>';
+    else
+        echo 'replace_dbDates succeeded</br>';
+    
+    $res = delete_dbDates($newDate);
+    if ($res == null)
+        echo 'Delete failed</br>';
 }
 //Created by James Loeffler
 function test_select_dbDates(){
@@ -69,4 +77,25 @@ function test_select_dbDates(){
     
     if (result != null)
         echo'select_dbDates failed</br>';
+    
+    $res = delete_dbDates($newDate);
+    if ($res == null)
+        echo 'Delete failed</br>';
+}
+
+function test_get_shifts_text(){
+    $newDate = new BSCAHdate("02-24-14", "test", "notes", "p1*p2");
+    $nd = insert_dbDates($newDate);
+    
+    echo 'will test get_shifts_text</br>';
+    $result = get_shifts_text($newDate);
+    
+    if (result != null)
+        echo'select_dbDates failed</br>';
+    else
+        echo'select_dbDates succeeded</br>';
+    
+    $res = delete_dbDates($newDate);
+    if ($res == null)
+        echo 'Delete failed</br>';
 }
