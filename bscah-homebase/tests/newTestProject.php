0bdf343e5678adc11439d63bae95e0e586942660
DerekDrago19 <derekdrago19@gmail.com>
2014-04-02 15:50:01 +0000

    Added fields to newTestProject

--- bscah-homebase/tests/newTestProject.php
+++ bscah-homebase/tests/newTestProject.php
@@ -11,15 +11,44 @@ function testConstructor()
 { 
 $test_project = new Project("03-12-14", "overnight", 10, 13, "weekly",15, "malcom1234567890+Malcom+Jones","matt1234567899+Matt+Smith", "Monday", "11-22-33-44-55", "notes");
         
-echo "Testing mm-dd-yy".'</br>';
+echo "Testing mm_dd_yy".'</br>';
 if($test_project->get_mm_dd_yy()=="03-12-14") 
-echo 'mm-dd-yy test succeeded </br>'; 
+echo 'mm_dd_yy test succeeded </br>'; 
 else
-echo 'mm-dd-yy test failed </br>';
+echo 'mm_dd_yy test failed </br>';
 
 if($test_project->get_name()=="overnight") 
 echo 'name test succeeded </br>'; 
 else
 echo 'name test failed </br>';
+
+if($test_project->get_start_time() == 10)
+    echo 'start time test succeeded </br>';
+else
+    echo 'start time test failed </br>';
+
+if ($test_project->get_end_time()==13)
+    echo 'end time test succeeded </br>';
+else
+    echo 'end time test failed </br>';
+
+if($test_project->get_venue() == "weekly")
+    echo 'venue test succeeded </br>';
+else
+    echo 'venue test failed </br>';
+if($test_project->get_vacancies() == 15)
+    echo 'vacancies test succeeded </br>';
+else
+    echo 'vacancies test failed </br>';
+
+if($test_project->get_persons() == "malcom1234567890+Malcom+Jones")
+    echo 'persons test succeeded </br>';
+else
+    echo 'persons test failed </br>';
+
+if($test_project->get_removed_persons() == "matt1234567899+Matt+Smith")
+    echo 'removed persons test succeeded </br>';
+else 
+    echo 'removed persons test failed </br>';
 
 }
