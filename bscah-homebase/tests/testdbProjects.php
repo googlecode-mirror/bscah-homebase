579398566ec199c2410debe5229b18ef3e780394
DerekDrago19 <derekdrago19@gmail.com>
2014-04-02 03:29:21 +0000

    Added a create and insert Project function to test for dbProjects

--- bscah-homebase/tests/testdbProjects.php
+++ bscah-homebase/tests/testdbProjects.php
@@ -1,96 +1,33 @@
 <?php
-/*
- * Created on Mar 18, 2014
- * Updated 3/26
- * @author Derek and Ka Ming
- */
-include_once(dirname(__FILE__) . '/../domain/Project.php');
-echo 'testing dbProjects.php</br>';
-testAddProject();
-testRetrieveProject();
+include_once(dirname(__FILE__) . '/../domain/project.php');
+include_once(dirname(__FILE__) . '/../database/dbProjects.php');
+echo 'testing dbProjectsphp</br>';
+error_log("in test db projects");
+test_create_dbProjects();
+echo("test complete</br>");
 
-//testGetall_dbProjects();
-
-echo("testdbProjects complete</br>");
-
-// tests the add_project() function in dbProjects.php
-function testAddProject() {
-    $m = new Project("Project 1" , "01-12-13", "13", "18");
-
-    echo 'will test add_project </br>';
-    $result = add_project($m);
+// tests the create_dbProjects() function in dbProjects.php
+function test_create_dbProjects()   {
+   
+   $newProject = new Project ("03-12-14", "overnight", 10, 13, "weekly",15, "malcom1234567890+Malcom+Jones","matt1234567899+Matt+Smith", "Monday", "11-22-33-44-55", "notes");
+    echo 'will test create_dbProjects </br>';
+    $result = create_dbProjects($newProject);
     echo 'result is ' . $result;
     if ($result)
-        echo "add_project succeeded </br>";
+        echo "create_dbProjects succeeded </br>";
     else
-        echo "add_project failed</br>";
-
-    $res = remove_project("Project1");
-    if ($res == null)
-        echo 'Retrieve failed</br>';
+        echo "create_dbProjects failed</br>";
 }
-
-// tests the retrieve_project() function in dbProjects.php
-function testRetrieveProject() {
-    $m = new Project("Project 1", "01-12-13", "13", "18");
-
-    echo 'will test retrieve_project </br>';
-    $result = add_project($m);
+function test_insert_dbProjects() {
+   
+    
+   $newProject= new Project("03-12-14", "overnight", 10, 13, "weekly",15, "malcom1234567890+Malcom+Jones","matt1234567899+Matt+Smith", "Monday", "11-22-33-44-55", "notes");
+    echo 'will test insert_dbProjects </br>';
+    $result = insert_dbProjects ($newProject);
     echo 'result is ' . $result;
     if ($result)
-        echo "add_project succeeded </br>";
+        echo "insert_dbProjects succeeded </br>";
     else
-        echo "add_project failed</br>";
-
-
-    $p = retrieve_project("Project1");
-    if ($p == null)
-        echo 'Retrieve failed</br>';
-    else {
-        checkEquals($p->get_status(), "active");
-       
-  
-    }
-
-    $res = remove_project("Project1");
-    if ($res == null)
-        echo 'Retrieve failed</br>';
-}
-
-// tests the retrieve_projects_by_name() function in dbProjects.php
-function testRetrieve_projects_by_name() {
-    $m = new Project("Project1" , "01-12-13", "13", "18");
-
-    echo 'will test retrieve_projects_by_name </br>';
-    $result = add_project($m);
-    echo 'result is ' . $result;
-    if ($result)
-        echo "add_project succeeded </br>";
-    else
-        echo "add_project failed</br>";
-
-    echo "test retrieve_projects_by_name</br>";
-    $projectList = retrieve_projects_by_name("Project1");
-    if ($projectList == null)
-        echo 'Retrieve failed</br>';
-    else {
-        checkEquals($projectList[0]->get_status(), "active");
-     
-    }
-
-    $res = remove_project("Project1");
-    if ($res == null)
-        echo 'Retrieve failed</br>';
-}  
-
-// test if the actual result equals the expected result
-function checkEquals($result, $expected) {
-    if ($result == $expected) {
-        echo 'result ' . $result . ' is the same as expected value ' . $expected . '</br>';
-        return true;
-    } else {
-        echo 'result ' . $result . ' does not equal the  expected value ' . $expected . '</br>';
-        return false;
-    }
+        echo "insert_dbProjects failed</br>";
 }
 ?>
